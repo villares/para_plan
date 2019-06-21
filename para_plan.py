270fb48e21e7d488c6b28ad020386c97f6f49e7c
villares <abav@lugaralgum.com>
2019-06-21 16:04:20 -0300

    v2

--- para_plan.py
+++ para_plan.py
@@ -6,12 +6,12 @@ def setup():
 	global img
 	createCanvas(1000, 500)
 	imageMode(CENTER)
-	img = loadImage('assets/ESPACO.png')
-	a1 = loadImage('assets/01.png')
+	img = loadImage('/assets/ESPACO.png')
+	# a1 = loadImage('assets/01.png')
 
 def draw():
 	w, h = img.width / 5, img.height / 5
-
+	rect(10, 10, 10, 10)
     image(img, width / 2, height /2 , 5, 5)
-    print("{} {}"
-    	.format(img.width, img.height))
+    # print("{} {}"
+    # 	.format(img.width, img.height))
