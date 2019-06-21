6aa89b913a5b9e19b971527667562a40cc05964a
villares <abav@lugaralgum.com>
2019-06-21 12:19:25 -0300

    1st

---
+++ para_plan.py
@@ -0,0 +1,17 @@
+from pytop5js import *
+
+img = None
+
+def setup():
+	global img
+	createCanvas(1000, 500)
+	imageMode(CENTER)
+	img = loadImage('assets/ESPACO.png')
+	a1 = loadImage('assets/01.png')
+
+def draw():
+	w, h = img.width / 5, img.height / 5
+
+    image(img, width / 2, height /2 , 5, 5)
+    print("{} {}"
+    	.format(img.width, img.height))
