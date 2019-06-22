7402d618975d51ffca70a2f44f027fd5f0caba4e
villares <abav@lugaralgum.com>
2019-06-21 22:44:15 -0300

    proto

--- jsversion/jsversion.js
+++ jsversion/jsversion.js
@@ -18,7 +18,8 @@ function setup() {
   mw = width / 2;
   mh = height / 2;
   imageMode(CENTER);
-  slider = createSlider(200, 800, 500);
+  rectMode(CENTER);
+  slider = createSlider(350, 750, 500);
   slider.position(10, 10);
   slider.style('width', '');
 }
@@ -26,24 +27,38 @@ function setup() {
 
 function draw() {
   background(200);
-  w = img.width;
-  h = img.height;
-  console.log( [iw(a1), iw(a2), iw(a3), iw(a4), iw(a5), iw(img)]
-    );
+  var W = iw(img);
+  //console.log( iw(prd));
+  fill(0);
+  rect(mw, mh, W - iw(prd)/2, ih(img)); 
   image(img, mw, mh, iw(img), ih(img) );
   var val = slider.value();
-  val = floor(val / 100)* 100;
-  text(val, 100, 100);
-  if (val < 100) {
-    image(a1, w / 2, mh, iw(a1), ih(a1) );
-  }
-  image(prd, mw - 500 + val, mh, iw(prd), ih(prd) );
+  val = floor(val / 50)* 50;
+  //text(val, 100, 100);
+  of = iw(prd) * 2;
+  off = iw(prd) * 4;
+  if (val <= 350) {
+    image(a1, - off + mw - W/2 + ih(a1)/2, mh, iw(a1), ih(a1) );
+    image(a5, - off + mw + W/2 - ih(a5)/2, mh, iw(a5), ih(a5) );
+  } else if (val <= 450) {
+    image(a2, - of + mw - W/2 + ih(a2)/2, mh, iw(a2), ih(a2) );
+    image(a4, - of + mw + W/2 - ih(a4)/2, mh, iw(a4), ih(a4) );
+  } else if (val <= 550) {
+    image(a3, mw - W/2 + ih(a3)/2, mh, iw(a3), ih(a3) );
+    image(a3, mw + W/2 - ih(a3)/2, mh, iw(a3), ih(a3) );
+  } else if (val <= 650) {
+    image(a4, + of + mw - W/2 + ih(a4)/2, mh, iw(a4), ih(a4) );
+    image(a2, + of + mw + W/2 - ih(a2)/2, mh, iw(a2), ih(a2) );
+  } else if (val <= 750) {
+    image(a5, + off + mw - W/2 + ih(a5)/2, mh, iw(a5), ih(a5) );
+    image(a1, + off + mw + W/2 - ih(a1)/2, mh, iw(a1), ih(a1) );
+  }   //image(prd, mw - 750 + val, mh, iw(prd), ih(prd) );
 }
 
 function ih(img) {
-  return img.height / 3;
+  return img.height / 4;
 }
 
 function iw(img) {
-  return img.width / 3;
+  return img.width / 4;
 }
