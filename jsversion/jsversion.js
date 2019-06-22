b6862b6175f05bced5ec8f102eb040b7f655defa
villares <abav@lugaralgum.com>
2019-06-21 23:10:32 -0300

    mirror

--- jsversion/jsversion.js
+++ jsversion/jsversion.js
@@ -15,8 +15,6 @@ function preload() {
 function setup() {
   //createCanvas(1200, 600);
   createCanvas(windowWidth, windowHeight);
-  mw = width / 2;
-  mh = height / 2;
   imageMode(CENTER);
   rectMode(CENTER);
   slider = createSlider(350, 750, 500);
@@ -26,39 +24,45 @@ function setup() {
 
 
 function draw() {
-  background(200);
+  translate(width / 2, height / 2)
+    background(200);
   var W = iw(img);
   //console.log( iw(prd));
   fill(0);
-  rect(mw, mh, W - iw(prd)/2, ih(img)); 
-  image(img, mw, mh, iw(img), ih(img) );
+  rect(0, 0, W - iw(prd)/2, ih(img)); 
+  image(img, 0, 0, iw(img), ih(img) );
   var val = slider.value();
   val = floor(val / 50)* 50;
   //text(val, 100, 100);
   of = iw(prd) * 2;
   off = iw(prd) * 4;
   if (val <= 350) {
-    image(a1, - off + mw - W/2 + ih(a1)/2, mh, iw(a1), ih(a1) );
-    image(a5, - off + mw + W/2 - ih(a5)/2, mh, iw(a5), ih(a5) );
+    image(a1, - off - W/2 + ih(a1)/2, 0, iw(a1), ih(a1) );
+    scale(-1, 1);
+    image(a5, + off - W/2 + ih(a5)/2, 0, iw(a5), ih(a5) );
   } else if (val <= 450) {
-    image(a2, - of + mw - W/2 + ih(a2)/2, mh, iw(a2), ih(a2) );
-    image(a4, - of + mw + W/2 - ih(a4)/2, mh, iw(a4), ih(a4) );
+    image(a2, - of - W/2 + ih(a2)/2, 0, iw(a2), ih(a2) );
+    scale(-1, 1);
+    image(a4, + of - W/2 + ih(a4)/2, 0, iw(a4), ih(a4) );
   } else if (val <= 550) {
-    image(a3, mw - W/2 + ih(a3)/2, mh, iw(a3), ih(a3) );
-    image(a3, mw + W/2 - ih(a3)/2, mh, iw(a3), ih(a3) );
+    image(a3, - W/2 + ih(a3)/2, 0, iw(a3), ih(a3) );
+    scale(-1, 1);
+    image(a3, - W/2 + ih(a3)/2, 0, iw(a3), ih(a3) );
   } else if (val <= 650) {
-    image(a4, + of + mw - W/2 + ih(a4)/2, mh, iw(a4), ih(a4) );
-    image(a2, + of + mw + W/2 - ih(a2)/2, mh, iw(a2), ih(a2) );
+    image(a4, + of - W/2 + ih(a4)/2, 0, iw(a4), ih(a4) );
+    scale(-1, 1);
+    image(a2, - of - W/2 + ih(a2)/2, 0, iw(a2), ih(a2) );
   } else if (val <= 750) {
-    image(a5, + off + mw - W/2 + ih(a5)/2, mh, iw(a5), ih(a5) );
-    image(a1, + off + mw + W/2 - ih(a1)/2, mh, iw(a1), ih(a1) );
+    image(a5, + off - W/2 + ih(a5)/2, 0, iw(a5), ih(a5) );
+    scale(-1, 1);
+    image(a1, - off - W/2 + ih(a1)/2, 0, iw(a1), ih(a1) );
   }   //image(prd, mw - 750 + val, mh, iw(prd), ih(prd) );
 }
 
 function ih(img) {
-  return img.height / 4;
+  return img.height / 6;
 }
 
 function iw(img) {
-  return img.width / 4;
+  return img.width / 6;
 }
